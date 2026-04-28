9508cad2b4c13920ff68283c2febce67505f30de
Sylvan Swierkosz <sswierkosz@gmail.com>
2026-04-28 13:07:05 -0700

    Remove screen recording guidance from onboarding

--- src/App.tsx
+++ src/App.tsx
@@ -252,10 +252,7 @@ function App() {
 
   if (onboardingStep === "accessibility") {
     return (
-      <AccessibilityOnboarding
-        onComplete={handleAccessibilityComplete}
-        showScreenRecordingGuidance={platform() === "macos"}
-      />
+      <AccessibilityOnboarding onComplete={handleAccessibilityComplete} />
     );
   }
 
